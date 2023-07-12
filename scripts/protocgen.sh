6316a7e21347647592dfecbeffd37d917960e8db
Dongri Jin <dongri.jin@speee.jp>
2023-07-12 16:34:24 +0900

    Fix ethereal.proto

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,28 +2,6 @@
 
 set -eo pipefail
 
-# proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-# for dir in $proto_dirs; do
-#   buf protoc \
-#   -I "proto" \
-#   -I "third_party/proto" \
-#   --gocosmos_out=plugins=interfacetype+grpc,\
-# Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-#   $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-#   # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-#   buf protoc \
-#   -I "proto" \
-#   -I "third_party/proto" \
-#   --grpc-gateway_out=logtostderr=true:. \
-#   $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-# done
-
-# cp -r github.com/datachainlab/ethereum-ibc-relay-prover/* ./
-# rm -rf github.com
-
-
 echo "Generating gogo proto code"
 cd proto
 
@@ -33,6 +11,7 @@ cd ..
 
 # move proto files to the right places
 cp -r github.com/datachainlab/ethereum-ibc-relay-prover/* ./
+cp -r github.com/datachainlab/ethereum-lcp/* ./
 rm -rf github.com
 
 go mod tidy
