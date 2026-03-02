1f43d43be5f8ed9f06d754d43402269167bc65c7
Daniel Izquierdo <daniel.izquierdo@speee.jp>
2026-03-02 15:07:55 +0900

    Include response body in failure log

--- beacon/client.go
+++ beacon/client.go
@@ -124,7 +124,7 @@ func (cl Client) get(ctx context.Context, path string, res any) error {
 		return err
 	}
 	if r.StatusCode < 200 || r.StatusCode >= 300 {
-		log.GetLogger().DebugContext(ctx, "Non 2xx response to Beacon API request", "endpoint", cl.endpoint+path, "status code", r.StatusCode)
+		log.GetLogger().DebugContext(ctx, "Non 2xx response to Beacon API request", "endpoint", cl.endpoint+path, "status code", r.StatusCode, "response body", string(bz))
 		return fmt.Errorf("request returned status code %d", r.StatusCode)
 	}
 	return json.Unmarshal(bz, &res)
